f4d9a278aa568b3145834d03a69ef606ec3b2a74
jmarrot <jonathan.marrot@gmail.com>
2014-02-25 13:53:01 +0100

    Fix check element TinyMCE form

--- current_version/library/CMS/Form/Element/TinyMCE.php
+++ current_version/library/CMS/Form/Element/TinyMCE.php
@@ -202,7 +202,8 @@ class CMS_Form_Element_TinyMCE extends CMS_Form_Element_Textarea
 				pdw_toggle_on : 1,
 				pdw_toggle_toolbars : "2,3",
 				// Configuration plugin spellchecker : Français par default
-				spellchecker_languages : "+French=fr, English=en"
+				spellchecker_languages : "+French=fr, English=en",
+				valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|name|href|target|title|class|onfocus|onblur],strong/b,em,i,strike,u,#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-h6,hr[size|noshade],-font[face|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],q[cite],samp,select[disabled|multiple|name|size],small,textarea[cols|rows|disabled|name|readonly],tt,var,big"
 			});';	
 			
         CMS_Application_ProcessLayout::getInstance()->appendJsScript($output);
